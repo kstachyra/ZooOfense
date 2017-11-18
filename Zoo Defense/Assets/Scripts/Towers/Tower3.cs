d8cd878734f5a8705f4a100710950cce620f3370
Seremonik <seremonik@gmail.com>
2017-11-18 17:45:59 +0100

    squirel

--- Zoo Defense/Assets/Scripts/Towers/Tower3.cs
+++ Zoo Defense/Assets/Scripts/Towers/Tower3.cs
@@ -1,19 +1,45 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower3 : Tower
 {
+    public GameObject nutPrefab;
 
-    // Use this for initialization
-    void Start()
+    public override void Attack()
     {
+        base.FocusEnemy();
+        GetComponent<Animator>().SetTrigger("Shoot");
+        StartCoroutine(WaitAndSpawnNut());
+    }
+
+    IEnumerator WaitAndSpawnNut()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject kula = Instantiate(nutPrefab);
+        kula.transform.position = transform.position;
+        if(focusedEnemy != null)
+        {
+            float distance = Vector3.Distance(transform.position, focusedEnemy.transform.position);
+            kula.transform.DOMove(focusedEnemy.transform.position + new Vector3(0, -0.2f, -0.5f), 0.01f * distance).SetEase(Ease.InSine).OnComplete(() =>
+            {
+                kula.transform.DOScale(Vector3.zero, 0.05f).OnComplete(() =>
+                {
+                    Destroy(kula);
+                    base.DealDamage();
+                });
+            });
+        }
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-
+        if(focusedEnemy != null)
+        {
+            var dir = (focusedEnemy.transform.position - transform.position).normalized;
+            transform.localScale = new Vector3(dir.x >= 0 ? 1 : -1, 1, 1);
+        }
     }
 }
