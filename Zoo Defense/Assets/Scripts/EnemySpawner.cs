f5a3dd1ca65c5aab9a228c127829296c53df02f7
KAPUT-Z500\KS <kacper.stachyra@gmail.com>
2017-11-18 14:19:02 +0100

    enemySpawner

--- Zoo Defense/Assets/Scripts/EnemySpawner.cs
+++ Zoo Defense/Assets/Scripts/EnemySpawner.cs
@@ -1,20 +1,95 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public Object Enemy;
+
+    public AnimationCurve wave;
+    public int roundTime;
+    public int enemies;
+    public int enemyTypes;
+
+    int toSpawn;
+    int spawned;
+
+    float timeFrame;
+    float startTime;
 
     int level = 0;
 
-    public void StartNewGame()
+    public void StartNewLevel()
     {
-        level = 0;
+        //TODO
+        toSpawn = enemies;
+        spawned = 0;
+
+
+        startTime = Time.time;
+        timeFrame = roundTime / 10;
+
+        StartCoroutine("SpawnEnemies");
     }
 
-    private void SpawnEnemy()
+    public void EndLevel()
     {
+        //TODO
+        level += 1;
 
+        UpdateUI();
+
+        StartNewLevel();
+    }
+
+    private void Start()
+    {
+        //!!! TODO
+        level = 0;
+        StartNewLevel();
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            float timePassed = (Time.time - startTime);
+            float timeLeft = roundTime - timePassed;
+            int howMany = (int)(((toSpawn - spawned) / timeLeft) * timeFrame);
+            howMany = (int)(howMany * UnityEngine.Random.Range(0.5f, 1.5f) * wave.Evaluate(timePassed / roundTime));
+            howMany++;
+            spawned += howMany;
+
+            for (int i = 0; i < howMany; ++i)
+            {
+                int enemyType = 0;
+
+                //upgrade enemy
+                for (int e = 0; e < enemyTypes; ++e)
+                {
+                    if (UnityEngine.Random.Range(0.0f, 2f) < wave.Evaluate(timePassed / roundTime))
+                    {
+                        enemyType++;
+                    }
+                }
+                StartCoroutine("SpawnEnemy", enemyType);
+            }
+
+            yield return new WaitForSeconds(timeFrame);
+
+            if (toSpawn - spawned <= 0)
+            {
+                EndLevel();
+                yield break;
+            }
+        }
+    }
+
+    private IEnumerator SpawnEnemy(int enemyType)
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0, timeFrame));
+        Debug.Log("Spawned: enemy " + enemyType);
+        Instantiate(Enemy);
+        yield break;
     }
 
     private void UpdateUI()
