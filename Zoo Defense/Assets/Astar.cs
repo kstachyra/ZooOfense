d0cb4634fcec7d3592d09fea70ac15882e1a7423
KAPUT-Z500\KS <kacper.stachyra@gmail.com>
2017-11-18 10:23:13 +0100

    Astar interface

--- Zoo Defense/Assets/Astar.cs
+++ Zoo Defense/Assets/Astar.cs
@@ -1,34 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Astar : MonoBehaviour
-{
-    public int columns;
-    public int rows;
-
-    public int startX;
-    public int startY;
-
-    Node end = new Node();
-    Node start = new Node();
-
-    bool[,] Grid;
-
-    HashSet<Node> ClosedSet = new HashSet<Node>();
-    HashSet<Node> OpenSet = new HashSet<Node>();
-
-    //poprzednik
-    Node[,] CameFrom;
-
-    //start -> node score
-    float[,] gScore;
-
-    //start -> node -> goal score
-    float[,] fScore;
-
-    class Node
+public class Node
     {
         public Node()
         {
@@ -70,28 +44,58 @@ public class Astar : MonoBehaviour
         public int Y { get; set; }
     };
 
+public static class Astar
+{
+    static int columns;
+    static int rows;
 
-    public void Init (int height, int width)
+    static Node end = new Node();
+    static Node start = new Node();
+
+    static bool[,] Grid;
+
+    static HashSet<Node> ClosedSet = new HashSet<Node>();
+    static HashSet<Node> OpenSet = new HashSet<Node>();
+
+    //poprzednik
+    static Node[,] CameFrom;
+
+    //start -> node score
+    static float[,] gScore;
+
+    //start -> node -> goal score
+    static float[,] fScore;
+
+
+    public static void Init (int width, int height)
     {
         columns = width;
         rows = height;
-
-        start = new Node(startX, startY);
 
         Grid = new bool[columns, rows];
 
         fScore = new float[columns, rows];
         gScore = new float[columns, rows];
         CameFrom = new Node[columns, rows];
+   	}
 
-        end.X = 0;
-        end.Y = rows / 2;
+    public static void SetGrid(bool[,] TowerGrid)
+    {
+        Grid = TowerGrid;
+    }
+
+    public static List<Node> CalcPath(Node s, Node e)
+    {
+        Clear();
+
+        start = new Node(s.X, s.Y);
+        end = new Node(e.X, e.Y);
 
         OpenSet.Add(new Node(start.X, start.Y));
 
-        for (int i=0; i<columns; ++i)
+        for (int i = 0; i < columns; ++i)
         {
-            for (int j=0; j<rows; ++j)
+            for (int j = 0; j < rows; ++j)
             {
                 gScore[i, j] = float.MaxValue;
                 fScore[i, j] = float.MaxValue;
@@ -101,16 +105,16 @@ public class Astar : MonoBehaviour
         }
 
         //start -> start = 0
-        gScore[startX, startY] = 0;
+        gScore[start.X, start.Y] = 0;
 
         //start - >start-> end score
-        fScore[startX, startY] = h(start, end);
+        fScore[start.X, start.Y] = H(start, end);
 
-        go();
-	}
 
-    private void go()
-    {
+
+
+
+
         Node best = new Node();
         while (OpenSet.Count > 0)
         {
@@ -131,8 +135,7 @@ public class Astar : MonoBehaviour
 
             if (best.Equals(end))
             {
-                printPath(start, end);
-                return;
+                return GetPath(start, end);
             }
 
             OpenSet.Remove(best);
@@ -144,8 +147,6 @@ public class Astar : MonoBehaviour
             neighbours.Add(new Node(best.X + 1, best.Y));
             neighbours.Add(new Node(best.X, best.Y -1));
             neighbours.Add(new Node(best.X, best.Y +1));
-
-
 
 
             foreach (Node node in neighbours)
@@ -168,7 +169,7 @@ public class Astar : MonoBehaviour
                             CameFrom[node.X, node.Y] = best;
 
                             gScore[node.X, node.Y] = newPath;
-                            fScore[node.X, node.Y] = gScore[node.X, node.Y] + h(node, end);
+                            fScore[node.X, node.Y] = gScore[node.X, node.Y] + H(node, end);
                         }
                     }
 
@@ -177,23 +178,45 @@ public class Astar : MonoBehaviour
 
         }
 
-
+        //nie ma ścieżki
+        return new List<Node>(); 
     }
 
-    private void printPath(Node start, Node end)
-    {
-        Node curr = end;
 
-        while (!curr.Equals(start))
+    private static void Clear()
+    {
+        OpenSet.Clear();
+        ClosedSet.Clear();
+
+        for (int i=0; i<columns; ++i)
         {
-            print(curr.X + "." + curr.Y);
+            for (int j=0; j<rows; ++j)
+            {
+                CameFrom[i, j] = null;
+            }
+        }
+    }
+
+    //@return lista wierzchołków ścieżki od końca do początku
+    private static List<Node> GetPath(Node s, Node e)
+    {
+        List<Node> path = new List<Node>();
+
+        Node curr = e;
+
+        while (curr !=null)
+        {
+            path.Add(curr);
+            Debug.Log("AAA" + curr.X + "." + curr.Y);
             curr = CameFrom[curr.X, curr.Y];
         }
-        print(start.X + "." + start.Y);
+
+        return path;
     }
 
+    
     //heurtstyczna odległość
-    private float h(Node from, Node to)
+    private static float H(Node from, Node to)
     {
         return Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y);
     }
